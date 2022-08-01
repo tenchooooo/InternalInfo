956753058570ec87b7e3915827519a7f82160b70
tenchooooo <tenchooo.227@gmail.com>
2022-08-01 01:16:45 +0000

    header作成

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -62,7 +62,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
-    root_path
+    top_path
   end
 
   protected
